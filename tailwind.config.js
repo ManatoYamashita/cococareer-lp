847a9756163249d159c15f0e60a9f17b8a5cd8fb
yamashita <g2172117@tcu.ac.jp>
2025-04-12 18:50:23 +0900

    FIX: ファイル構造の修正

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#1F2124',
+      'main': "#F6B352",
+      'midnightyellow': '#001B50',
+      'electricyellow' : '#F6B352',
+      'grey500': '#ECECEC',
+      'yellow' : "#f9c00c",
+      'lightgrey' : "#A8AEB9",
+      'revertyellow' : "#f6ea8c",
+      'translucentwhite' : "rgba(255, 255, 255, 0.65)",
+    },
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.25rem' }],
+      base: ['1rem', { lineHeight: '1.5rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '1' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
+      '85xl': ['85px', { lineHeight: '1' }],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
